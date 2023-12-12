0996ad577d6b4f03b75f32499f082a6e89b8c5f9
Elizabeth <tjeudy.e@gmail.com>
2023-12-11 19:48:34 -0500

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Header from "./Components/Header";
+import Center from "./Components/Center";
 
 function App() {
-	return <div></div>;
+	return (
+		<div>
+			{/* Header section */}
+			<Header />;{/* Center section */}
+			<Center />;
+		</div>
+	);
 }
 
 export default App;
